0104c0958cca77be8ca421cd0208ab29e62ace6e
Jonah George <jonah.george@me.com>
2017-07-29 11:32:54 -0400

    Fix signup flow

--- users_handler.go
+++ users_handler.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/flosch/pongo2"
 	"net/http"
 )
@@ -12,18 +13,22 @@ func (app *Application) UsersNewHandler(w http.ResponseWriter, r *http.Request) 
 func (app *Application) UsersCreateHandler(w http.ResponseWriter, r *http.Request) {
 	session, _ := app.GetSession(r)
 
-	_, err := NewUsersRepository(app.db).Create(
+	user, err := NewUsersRepository(app.db).Create(
 		r.PostFormValue("name"),
 		r.PostFormValue("email"),
 		r.PostFormValue("password"))
+
 	if err != nil {
 		session.AddFlash("Woah, something bad happened.")
+		session.Save(r, w)
 		app.Render(w, r, "users/new", pongo2.Context{
 			"name":  r.PostFormValue("name"),
 			"email": r.PostFormValue("email"),
 		})
 	}
 
-	session.AddFlash("Successfully signed up!")
+	session.Values["userId"] = user.id
+	session.AddFlash(fmt.Sprintf("Welcome, %s!", user.name))
+	session.Save(r, w)
 	http.Redirect(w, r, "/", 302)
 }
