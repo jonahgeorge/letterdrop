01902e0c274829ef496ba91989efde350cf016a2
Jonah George <jonah.george@me.com>
2017-07-29 17:17:35 -0400

    Refactor UsersHandler

--- users_handler.go
+++ users_handler.go
@@ -2,8 +2,9 @@ package main
 
 import (
 	"fmt"
-	"github.com/flosch/pongo2"
 	"net/http"
+
+	"github.com/flosch/pongo2"
 )
 
 func (app *Application) UsersNewHandler(w http.ResponseWriter, r *http.Request) {
@@ -13,18 +14,18 @@ func (app *Application) UsersNewHandler(w http.ResponseWriter, r *http.Request) 
 func (app *Application) UsersCreateHandler(w http.ResponseWriter, r *http.Request) {
 	session, _ := app.GetSession(r)
 
-	user, err := NewUsersRepository(app.db).Create(
-		r.PostFormValue("name"),
-		r.PostFormValue("email"),
-		r.PostFormValue("password"))
+	newUser := &User{
+		name:           r.PostFormValue("name"),
+		email:          r.PostFormValue("email"),
+		passwordDigest: r.PostFormValue("password"),
+	}
 
+	user, err := NewUsersRepository(app.db).Create(newUser.name, newUser.email, newUser.passwordDigest)
 	if err != nil {
 		session.AddFlash("Woah, something bad happened.")
 		session.Save(r, w)
-		app.Render(w, r, "users/new", pongo2.Context{
-			"name":  r.PostFormValue("name"),
-			"email": r.PostFormValue("email"),
-		})
+		app.Render(w, r, "users/new", pongo2.Context{"user": newUser})
+		return
 	}
 
 	session.Values["userId"] = user.id
